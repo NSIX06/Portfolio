41b6abf0dc5949615b7d09e43ec2b6522be78c51
Luiz Felipe Pablos Bugalho <133234606+NSIX06@users.noreply.github.com>
2026-03-05 10:52:58 -0400

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -37,7 +37,7 @@ export default function Footer() {
       <div className={`container ${styles.bottom}`}>
         <p className={styles.copy}>
           © {year}{' '}
-          <span className={styles.copyAccent}>Luiz Felipe Bugalho</span> — NSIX06
+          <span className={styles.copyAccent}>Luiz Felipe Pablos Bugalho</span> — NSIX06
         </p>
         <p className={styles.copy}>
           Feito com <span className={styles.copyAccent}>♥</span> em Rondonópolis&nbsp;-&nbsp;MT ☕
